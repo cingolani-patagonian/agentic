ed241abb3cd75650a1312e16d97913691e4d9849
Federico Cingolani <cingolani@cingolanifede.local>
2026-01-15 21:57:33 -0300

    test_runner: feature: add server implementation with tests

---
+++ app/server/server.py
@@ -0,0 +1,5 @@
+"""FastAPI server placeholder.
+
+This file is a placeholder for the FastAPI server implementation.
+It will be implemented in future issues.
+"""
---
+++ app/server/tests/__init__.py
@@ -0,0 +1,5 @@
+"""Test package for the server application.
+
+This package contains tests for all backend functionality.
+Tests will be implemented alongside feature development.
+"""
